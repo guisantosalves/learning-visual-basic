1ee531cffd7d3a895ccb0bf74652515eebf2df46
guisantosalves <workguisantos@gmail.com>
2022-07-10 13:59:24 -0400

    conditional and loops is done

---
+++ Form1.Designer.vb
@@ -0,0 +1,248 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnMostraValores = New System.Windows.Forms.Button()
+        Me.btnMostraMat = New System.Windows.Forms.Button()
+        Me.btnDecimal = New System.Windows.Forms.Button()
+        Me.btnIF = New System.Windows.Forms.Button()
+        Me.txtNumber = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnVerifyName = New System.Windows.Forms.Button()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnVerifyHeight = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtHeight = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.btnLoopFor = New System.Windows.Forms.Button()
+        Me.lstDados = New System.Windows.Forms.ListBox()
+        Me.btnLoopWhile = New System.Windows.Forms.Button()
+        Me.lstDadosWhile = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'btnMostraValores
+        '
+        Me.btnMostraValores.Location = New System.Drawing.Point(73, 23)
+        Me.btnMostraValores.Name = "btnMostraValores"
+        Me.btnMostraValores.Size = New System.Drawing.Size(166, 29)
+        Me.btnMostraValores.TabIndex = 0
+        Me.btnMostraValores.Text = "Mostra Valores"
+        Me.btnMostraValores.UseVisualStyleBackColor = True
+        '
+        'btnMostraMat
+        '
+        Me.btnMostraMat.Location = New System.Drawing.Point(73, 77)
+        Me.btnMostraMat.Name = "btnMostraMat"
+        Me.btnMostraMat.Size = New System.Drawing.Size(166, 29)
+        Me.btnMostraMat.TabIndex = 1
+        Me.btnMostraMat.Text = "matematica"
+        Me.btnMostraMat.UseVisualStyleBackColor = True
+        '
+        'btnDecimal
+        '
+        Me.btnDecimal.Location = New System.Drawing.Point(73, 125)
+        Me.btnDecimal.Name = "btnDecimal"
+        Me.btnDecimal.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.btnDecimal.Size = New System.Drawing.Size(166, 29)
+        Me.btnDecimal.TabIndex = 2
+        Me.btnDecimal.Text = "teste com decimal"
+        Me.btnDecimal.UseVisualStyleBackColor = True
+        '
+        'btnIF
+        '
+        Me.btnIF.Location = New System.Drawing.Point(73, 181)
+        Me.btnIF.Name = "btnIF"
+        Me.btnIF.Size = New System.Drawing.Size(166, 29)
+        Me.btnIF.TabIndex = 3
+        Me.btnIF.Text = "IF decision"
+        Me.btnIF.UseVisualStyleBackColor = True
+        '
+        'txtNumber
+        '
+        Me.txtNumber.Location = New System.Drawing.Point(308, 183)
+        Me.txtNumber.Name = "txtNumber"
+        Me.txtNumber.Size = New System.Drawing.Size(178, 27)
+        Me.txtNumber.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(308, 160)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(118, 20)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Digite o numero"
+        '
+        'btnVerifyName
+        '
+        Me.btnVerifyName.Location = New System.Drawing.Point(73, 279)
+        Me.btnVerifyName.Name = "btnVerifyName"
+        Me.btnVerifyName.Size = New System.Drawing.Size(166, 29)
+        Me.btnVerifyName.TabIndex = 6
+        Me.btnVerifyName.Text = "Verifica nome"
+        Me.btnVerifyName.UseVisualStyleBackColor = True
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(308, 281)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(167, 27)
+        Me.txtName.TabIndex = 7
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(308, 258)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(118, 20)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Digite seu nome"
+        '
+        'btnVerifyHeight
+        '
+        Me.btnVerifyHeight.Location = New System.Drawing.Point(589, 55)
+        Me.btnVerifyHeight.Name = "btnVerifyHeight"
+        Me.btnVerifyHeight.Size = New System.Drawing.Size(128, 29)
+        Me.btnVerifyHeight.TabIndex = 9
+        Me.btnVerifyHeight.Text = "verify"
+        Me.btnVerifyHeight.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(544, 77)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(0, 20)
+        Me.Label3.TabIndex = 10
+        '
+        'Label4
+        '
+        Me.Label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label4.Location = New System.Drawing.Point(550, 15)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(10, 643)
+        Me.Label4.TabIndex = 11
+        '
+        'txtHeight
+        '
+        Me.txtHeight.Location = New System.Drawing.Point(753, 57)
+        Me.txtHeight.Name = "txtHeight"
+        Me.txtHeight.Size = New System.Drawing.Size(220, 27)
+        Me.txtHeight.TabIndex = 12
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(753, 32)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(117, 20)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "type your height"
+        '
+        'btnLoopFor
+        '
+        Me.btnLoopFor.Location = New System.Drawing.Point(589, 136)
+        Me.btnLoopFor.Name = "btnLoopFor"
+        Me.btnLoopFor.Size = New System.Drawing.Size(128, 29)
+        Me.btnLoopFor.TabIndex = 14
+        Me.btnLoopFor.Text = "loop for"
+        Me.btnLoopFor.UseVisualStyleBackColor = True
+        '
+        'lstDados
+        '
+        Me.lstDados.FormattingEnabled = True
+        Me.lstDados.ItemHeight = 20
+        Me.lstDados.Location = New System.Drawing.Point(730, 136)
+        Me.lstDados.Name = "lstDados"
+        Me.lstDados.Size = New System.Drawing.Size(270, 184)
+        Me.lstDados.TabIndex = 15
+        '
+        'btnLoopWhile
+        '
+        Me.btnLoopWhile.Location = New System.Drawing.Point(589, 387)
+        Me.btnLoopWhile.Name = "btnLoopWhile"
+        Me.btnLoopWhile.Size = New System.Drawing.Size(128, 29)
+        Me.btnLoopWhile.TabIndex = 16
+        Me.btnLoopWhile.Text = "loop while"
+        Me.btnLoopWhile.UseVisualStyleBackColor = True
+        '
+        'lstDadosWhile
+        '
+        Me.lstDadosWhile.FormattingEnabled = True
+        Me.lstDadosWhile.ItemHeight = 20
+        Me.lstDadosWhile.Location = New System.Drawing.Point(730, 387)
+        Me.lstDadosWhile.Name = "lstDadosWhile"
+        Me.lstDadosWhile.Size = New System.Drawing.Size(270, 264)
+        Me.lstDadosWhile.TabIndex = 17
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1075, 667)
+        Me.Controls.Add(Me.lstDadosWhile)
+        Me.Controls.Add(Me.btnLoopWhile)
+        Me.Controls.Add(Me.lstDados)
+        Me.Controls.Add(Me.btnLoopFor)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtHeight)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnVerifyHeight)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.btnVerifyName)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtNumber)
+        Me.Controls.Add(Me.btnIF)
+        Me.Controls.Add(Me.btnDecimal)
+        Me.Controls.Add(Me.btnMostraMat)
+        Me.Controls.Add(Me.btnMostraValores)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnMostraValores As Button
+    Friend WithEvents btnMostraMat As Button
+    Friend WithEvents btnDecimal As Button
+    Friend WithEvents btnIF As Button
+    Friend WithEvents txtNumber As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnVerifyName As Button
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnVerifyHeight As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtHeight As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents btnLoopFor As Button
+    Friend WithEvents lstDados As ListBox
+    Friend WithEvents btnLoopWhile As Button
+    Friend WithEvents lstDadosWhile As ListBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,194 @@
+﻿Public Class Form1
+    Private Sub btnMostraValores_Click(sender As Object, e As EventArgs) Handles btnMostraValores.Click
+
+        Dim intIdade As Integer = 42
+        Dim dblAltura As Double = 1.65
+        Dim strNome As String = "guizaodozap"
+        Dim bolHasChild As Boolean = True
+
+
+        MsgBox(intIdade)
+        MsgBox(dblAltura)
+        MsgBox(strNome)
+        MsgBox(bolHasChild)
+
+    End Sub
+
+    Private Sub btnMostraMat_Click(sender As Object, e As EventArgs) Handles btnMostraMat.Click
+
+        Dim intNumber As Integer
+
+        'soma
+        intNumber = 20
+        intNumber = intNumber + 5
+
+        MessageBox.Show("the number with plus five is: " & intNumber.ToString, "Soma")
+
+        'subtração
+        intNumber = 20
+        intNumber = intNumber - 5
+
+        MessageBox.Show("the number with less five is: " & intNumber.ToString, "subtracao")
+
+        'multiplicação
+        intNumber = 20
+        intNumber = intNumber * 5
+
+        MessageBox.Show("the number with 5 times is: " & intNumber.ToString, "multiplicacao")
+
+        'divisao
+        intNumber = 20
+        intNumber = intNumber / 5
+
+        MessageBox.Show("the number divided to 5 is: " & intNumber.ToString, "divisao")
+
+        'resto de divisao
+        intNumber = 20
+        intNumber = intNumber Mod 5
+
+        MessageBox.Show("the mod of 20 for 5: " & intNumber.ToString, "mod")
+
+    End Sub
+
+    Private Sub btnDecimal_Click(sender As Object, e As EventArgs) Handles btnDecimal.Click
+
+        Dim dblNumber As Double
+
+        'mult
+        dblNumber = 25.23
+
+        dblNumber *= 5.6
+
+        MessageBox.Show("Im testing the double type: " & dblNumber.ToString("#.##"), "double type")
+
+        'div
+        dblNumber = 15
+
+        dblNumber /= 6
+
+        MessageBox.Show("Im testing the double type: " & dblNumber.ToString("#.##"), "double type")
+    End Sub
+
+    Private Sub btnIF_Click(sender As Object, e As EventArgs) Handles btnIF.Click
+
+        'CDbl is a way to convert data to double
+        Dim intNumber As Integer = CDbl(txtNumber.Text)
+
+        If intNumber = 27 Then
+
+            MessageBox.Show($"the number is equals: {intNumber}", "if else")
+
+        ElseIf intNumber = 2 Then
+
+            MessageBox.Show($"the number {intNumber} is diferent of 27")
+
+        Else
+            MessageBox.Show($"the number {intNumber} is diferent of both")
+        End If
+
+
+    End Sub
+
+    Private Sub btnVerifyName_Click(sender As Object, e As EventArgs) Handles btnVerifyName.Click
+
+        Dim StrName As String = txtName.Text
+
+        'If StrName = "guizaodozap" Then
+
+        '    MessageBox.Show($"you're the super {StrName} bruhh")
+
+        'ElseIf StrName = "gabriel" Then
+
+        '    MessageBox.Show($"you're only the {StrName} cof cof")
+
+        'Else
+
+        '    MessageBox.Show("you're nothing bruh, get out of here")
+
+        'End If
+
+        ' <> -> means diferent like !=
+
+        If StrName <> "guizaodozap" Then
+
+            MessageBox.Show("you're nothing")
+
+        ElseIf StrName = "guizaodozap" Then
+
+            MessageBox.Show($"you're so handsome {StrName}")
+
+        Else
+            MessageBox.Show("type anything right")
+
+        End If
+
+    End Sub
+
+    Private Sub btnVerifyHeight_Click(sender As Object, e As EventArgs) Handles btnVerifyHeight.Click
+        'dado em metros
+
+        'using try catch
+        Dim dblHeight As Double
+
+        Try
+            dblHeight = CDbl(txtHeight.Text)
+        Catch
+            Exit Sub
+        End Try
+
+
+        If dblHeight > 0 And dblHeight <= 1 Then
+
+            MessageBox.Show("You're a dwarf !!", "Dwarf")
+
+        ElseIf dblHeight > 1 And dblHeight <= 1.5 Then
+
+            MessageBox.Show("You're almost a dwarf !!", "almost Dwarf")
+
+        ElseIf dblHeight > 1.5 And dblHeight <= 2 Then
+
+            MessageBox.Show("You're a normal person!!", "normal person")
+
+        Else
+            MessageBox.Show("type right values", "warning")
+        End If
+
+    End Sub
+
+    Private Sub btnLoopFor_Click(sender As Object, e As EventArgs) Handles btnLoopFor.Click
+
+        Dim intConta As Integer
+
+        'calling an another routine
+        LimparLista()
+
+        'puting data in the list
+        For intConta = 1 To 10
+            lstDados.Items.Add($" 9 x {intConta.ToString} = {9 * intConta}")
+        Next
+
+    End Sub
+
+    Private Sub LimparLista()
+        'cleaning the list
+        lstDados.Items.Clear()
+
+        lstDadosWhile.Items.Clear()
+
+    End Sub
+
+    Private Sub btnLoopWhile_Click(sender As Object, e As EventArgs) Handles btnLoopWhile.Click
+        Dim intCount As Integer = 0
+
+        'calling another subroutine
+        LimparLista()
+
+        Do While intCount < 11
+
+            lstDadosWhile.Items.Add($"5 x {intCount.ToString} = {5 * intCount}")
+
+            intCount += 1
+        Loop
+
+    End Sub
+End Class
